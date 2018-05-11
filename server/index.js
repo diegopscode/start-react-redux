5ca388ba352f30f8b3bda8e6c4a1b443186bb993
diego pancera <diegopancera@MacBook-Pro-de-open.local>
2018-05-11 16:58:09 -0300

    Update server

--- server/index.js
+++ server/index.js
@@ -1,37 +1,64 @@
-import express from "express";
 import path from "path";
+import Express from "express";
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import React from "react";
 import { renderToString } from "react-dom/server";
-import Home from "../src/containers/Home";
 
-const app = express();
+import reducers from '../src/reducers'
 
-app.use( express.static( path.resolve( __dirname, "../public" ) ) );
+import App from "../src/App";
 
-app.get( "/*", ( req, res ) => {
-    const jsx = ( <Home /> );
-    const reactDom = renderToString( jsx );
+const app = Express()
+const port = 3000
 
-    res.writeHead( 200, { "Content-Type": "text/html" } );
-    res.end( htmlTemplate( reactDom ) );
-} );
+app.use( Express.static( path.resolve( __dirname, "../build" ) ) );
 
-app.listen( 2048 );
+// app.get( "/*", ( req, res ) => {
+//     const jsx = ( <App /> );
+//     const reactDom = renderToString( jsx );
 
-function htmlTemplate( reactDom ) {
+//     res.writeHead( 200, { "Content-Type": "text/html" } );
+//     res.end( htmlTemplate( reactDom ) );
+// } );
+
+app.listen( port );
+
+function renderFullPage(html, preloadedState) {
     return `
-        <!DOCTYPE html>
-        <html>
+      <!doctype html>
+      <html>
         <head>
-            <meta charset="utf-8">
-            <title>React SSR</title>
+          <title>Redux SRR Example</title>
         </head>
-        
         <body>
-            <div id="app">${ reactDom }</div>
-            <script src="./app.bundle.js"></script>
+          <div id="root">${html}</div>
+          <script>
+            // WARNING: See the following for security issues around embedding JSON in HTML:
+            // http://redux.js.org/recipes/ServerRendering.html#security-considerations
+            window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+          </script>
+          <script src="/static/bundle.js"></script>
         </body>
-        </html>
-    `;
+      </html>
+    `
+}
+
+function handleRender(req, res) {
+    // Create a new Redux store instance
+    const store = createStore(reducers)
+  ​
+    // Render the component to a string
+    const html = renderToString(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  ​
+    // Grab the initial state from our Redux store
+    const preloadedState = store.getState()
+  ​
+    // Send the rendered page back to the client
+    res.send(renderFullPage(html, preloadedState))
 }
